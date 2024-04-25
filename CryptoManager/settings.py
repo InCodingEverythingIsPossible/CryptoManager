c477c8cc50141d243ba0a5590abd928ceab4dd24
Patryk Wojciechowski <patryk5wojciechowski@gmail.com>
2024-04-25 22:11:57 +0200

    Initialize project

--- CryptoManager/settings.py
+++ CryptoManager/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.messages',
+    'django.contrib.sessions',
     'django.contrib.staticfiles',
 ]
 
